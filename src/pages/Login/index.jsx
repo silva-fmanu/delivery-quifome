83c2d932eb19329e1493725b32003861f83e461e
julia-is-awake <juliasorgetzalves@gmail.com>
2024-11-29 15:16:18 -0300

    Página de login com verificação e salvamento no sessao

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,33 +1,81 @@
+import { useEffect, useState } from "react";
 import Logo from "../../assets/logo.png";
 import "./login.css";
+import api from "../../services/api";
+import { useNavigate } from "react-router-dom";
 
 function Login() {
+
+  const [email, setEmail] = useState(null);
+  const [senha, setSenha] = useState(null);
+  const [mensagem, setMensagem] = useState(null);
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if(sessionStorage.getItem("sessao")){
+      navigate("/delivery-quifome/home")
+    }
+  }, [])
+
+  const Login = async (e) => {
+    e.preventDefault();
+
+    try{
+
+      let usuarios = await api.get("/users");
+      usuarios = usuarios.data;
+
+      usuarios.forEach((item) => {
+        if(item.email === email){
+
+          if(item.senha === senha){
+
+            sessionStorage.setItem("sessao", JSON.stringify(item));
+            navigate("/delivery-quifome/home");
+
+          }
+        }else{
+
+          setMensagem("Email ou senha incorretos!")
+
+        }
+      })
+    } catch(err) {
+      console.log(err);
+    }
+  }
+
+
   return (
     <div>
       <div>
         <img src={Logo} alt="" />
-        <form>
-          <div class="form-group">
+        <form onSubmit={Login}>
+          <div className="form-group">
             <label className="amarelo">Email:</label>
             <input
               type="email"
-              class="form-control"
+              className="form-control"
               aria-describedby="emailHelp"
               placeholder="Enter email"
+              onChange={(e) => setEmail(e.target.value)}
             />
           </div>
-          <div class="form-group">
+          <div className="form-group">
             <label className="amarelo">Password</label>
             <input
               type="password"
-              class="form-control"
+              className="form-control"
               placeholder="Password"
+              onChange={(e) => setSenha(e.target.value)}
             />
           </div>
-          <button type="submit" class="btn botao-login mt-5">
+          <button type="submit" className="btn botao-login mt-5">
             Login
           </button>
         </form>
+        <p>{mensagem}</p>
       </div>
     </div>
   );
