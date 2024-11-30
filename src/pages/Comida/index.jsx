220274613399988edd467b4f8cb24c210b90adb9
silva-fmanu <silvafarias.manu07@gmail.com>
2024-11-29 23:10:05 -0300

    tela de comida card individual

--- src/pages/Comida/index.jsx
+++ src/pages/Comida/index.jsx
@@ -4,10 +4,12 @@ import "./comida.css";
 import HomeFosco from '../../assets/Homefosco.png';
 import profileFosco from '../../assets/profileFosco.png';
 import {Link} from "react-router-dom";
+import Hamburger from '../../assets/hamburger.jpg';
+import Star from '../../assets/StarFill.png';
 
 function Comida(){
 
-    const [comida, setComida] = useState();
+    /*const [comida, setComida] = useState();
 
     useEffect(() => {
       getData();
@@ -24,28 +26,41 @@ function Comida(){
       } catch (err) {
         console.log(err);
       }
-    };
+    };*/
 
     return(
-        <div className="body-comida">
+        <div className="body-all">
             <Cabecalho/>
-
-            
-
-            <div className="tabs">
-                <ul className="nav">
-                    <li className="nav-item">
-                        <Link className="nav-link" to="/delivery-quifome/home">
-                            <img src={HomeFosco} alt="Ícone home" />
-                        </Link>
-                    </li>
-                    <li className="nav-item">
-                        <Link className="nav-link" to="/delivery-quifome/perfil">
-                            <img src={profileFosco} alt="Ícone de perfil" />
-                        </Link>
-                    </li>
-                </ul>
+            <div className="body-comida">
+                <div className="card card-comida" style={{ width: '18rem' }}>
+                    <img src={Hamburger} className="card-img-top imgComida" alt="" />
+                    <div className="card-body">
+                        <h5 className="card-title">Hamburger</h5>
+                        <p className="card-text">Tempo de preparo: 30-40min</p>
+                        <p className="card-text">Preço: $9,99</p>
+                        <div className="rating">
+                            <img src={Star} alt="" className="starImg"/>
+                            <p className="card-text">4,5</p>
+                        </div>
+                        
+                        <a href="#" className="btn btn-primary btnComprar">Comprar</a>
+                    </div>
+                </div>    
             </div>
+                <div className="tabs">
+                    <ul className="nav">
+                        <li className="nav-item">
+                            <Link className="nav-link" to="/delivery-quifome/home">
+                                <img src={HomeFosco} alt="Ícone home" />
+                            </Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link className="nav-link" to="/delivery-quifome/perfil">
+                                <img src={profileFosco} alt="Ícone de perfil" />
+                            </Link>
+                        </li>
+                    </ul>
+                </div>
         </div>
     );
 }
--- src/routes.jsx
+++ src/routes.jsx
@@ -15,7 +15,7 @@ function RoutesApp() {
                 <Route path="/delivery-quifome/home" element={<Home/>}/>
                 <Route path="/delivery-quifome/perfil" element={<Perfil/>}/>
                 <Route path="/delivery-quifome/comidas" element={<Comidas/>}/>
-                <Route path="/delivery-quifome/comida/:ComidaId" element={<Comida/>}/>
+                <Route path="/delivery-quifome/comida" element={<Comida/>}/>
                 <Route path="/delivery-quifome/restaurantes" element={<Restaurantes/>}/>
                 <Route path="/delivery-quifome/restaurante/:RestauranteId" element={<Restaurante/>}/>
             </Routes>
